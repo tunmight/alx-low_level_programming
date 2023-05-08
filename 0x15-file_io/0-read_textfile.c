26e769f1faa957f4470205fc4a7ebc7f21380ab5
tunmight <tunmight111@gmail.com>
2023-05-08 22:13:47 +0100

    fileio

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,42 +12,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        ssize_t fd, num_read, num_written;
-        char *buffer;
+	ssize_t fd, num_read, num_written;
+	char *buffer;
 
-        if (filename == NULL)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        buffer = malloc(sizeof(char) * (letters + 1));
-        if (buffer == NULL)
-                return (0);
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+		return (0);
 
-        fd = open(filename, O_RDONLY);
-        if (fd == -1)
-        {
-                free(buffer);
-                return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	num_read = read(fd, buffer, letters);
+	if (num_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
         }
 
-        num_read = read(fd, buffer, letters);
-        if (num_read == -1)
-        {
-                free(buffer);
-                close(fd);
-                return (0);
-        }
+	buffer[num_read] = '\0';
 
-        buffer[num_read] = '\0';
+	num_written = write(STDOUT_FILENO, buffer, num_read);
+	if (num_written == -1 || num_written != num_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+		}
 
-        num_written = write(STDOUT_FILENO, buffer, num_read);
-        if (num_written == -1 || num_written != num_read)
-        {
-                free(buffer);
-                close(fd);
-                return (0);
-        }
-
-        free(buffer);
-        close(fd);
-        return (num_written);
+	free(buffer);
+	close(fd);
+	return (num_written);
 }
